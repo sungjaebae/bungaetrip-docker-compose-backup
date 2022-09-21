80cae8deddae5905641f310e63afc29418552e7d
Hank Sungjae Bae <hsm0156@gmail.com>
2022-09-21 17:33:36 +0900

    local->dev로 명칭 변경, 리액트 docker에서 제외

--- certificate-generator.production.sh
+++ certificate-generator.production.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-SSL_Path=/etc/letsencrypt/live/gogetter.kr
 domain=gogetter.kr
+SSL_Path=/etc/letsencrypt/live/$domain
 
 mkdir -p ./certbot/conf/live/$domain
 
@@ -12,7 +11,6 @@ docker-compose -f docker-compose.production.yml run --rm --entrypoint "\
     -keyout '$SSL_Path/privkey.pem' \
     -out '$SSL_Path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
-
 docker-compose -f docker-compose.production.yml up --force-recreate -d nginx
 
 docker-compose -f docker-compose.production.yml run --rm --entrypoint "\
@@ -20,11 +18,11 @@ docker-compose -f docker-compose.production.yml run --rm --entrypoint "\
   rm -rf /etc/letsencrypt/archive/$domain && \
   rm -rf /etc/letsencrypt/renewal/$domain.conf" certbot
 
-  docker-compose -f docker-compose.production.yml  run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
+docker-compose -f docker-compose.production.yml  run --rm --entrypoint "\
+    certbot certonly --webroot -w /var/www/certbot \
     --email hsm0156@gmail.com \
-    -d gogetter.kr \
-    -d www.gogetter.kr \
+    -d $domain \
+    -d www.$domain \
     --rsa-key-size 4096 \
     --agree-tos \
     --no-eff-email \
