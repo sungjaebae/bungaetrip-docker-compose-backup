962f14d64c5c1c45860d332f1f48da336e99d76d
Hank Sungjae Bae <hsm0156@gmail.com>
2022-10-13 19:47:46 +0900

    certificate-generator.dev.sh 추가

---
+++ certificate-generator.dev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#localhost 인증서만 생성함
+domain=localhost
+SSL_Path=/etc/letsencrypt/live/$domain
+
+mkdir -p ./certbot/conf/live/$domain
+
+
+docker-compose -f docker-compose.dev.yml run --rm --entrypoint "\
+  rm -rf /etc/letsencrypt/live/$domain && \
+  rm -rf /etc/letsencrypt/archive/$domain && \
+  rm -rf /etc/letsencrypt/renewal/$domain.conf" certbot
+
+docker-compose -f docker-compose.dev.yml run --rm --entrypoint "\
+    openssl req -x509 -nodes \
+    -days 365 \
+    -newkey rsa:2048 \
+    -keyout '$SSL_Path/privkey.pem' \
+    -out '$SSL_Path/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
+docker-compose -f docker-compose.dev.yml up --force-recreate -d nginx
