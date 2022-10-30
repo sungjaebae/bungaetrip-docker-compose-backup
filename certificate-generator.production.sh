033674be5ea33fe4ad113ffbd1adcfcadd4f5716
Ubuntu <azureuser@bungaetrip-api-server.rptkdonoraputoy4yxwd1qkc3f.syx.internal.cloudapp.net>
2022-10-30 10:37:36 +0000

    production서버 bungaetrip.com 호스팅 기능 추가

--- certificate-generator.production.sh
+++ certificate-generator.production.sh
@@ -11,6 +11,21 @@ docker-compose -f docker-compose.production.yml run --rm --entrypoint "\
     -keyout '$SSL_Path/privkey.pem' \
     -out '$SSL_Path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
+
+
+#### bungaetrip.com 인증서 받기
+domain2=bungaetrip.com
+SSL_Path2=/etc/letsencrypt/live/$domain2
+
+mkdir -p ./certbot/conf/live/$domain2
+
+docker-compose -f docker-compose.production.yml run --rm --entrypoint "\
+    openssl req -x509 -nodes \
+    -days 365 \
+    -newkey rsa:2048 \
+    -keyout '$SSL_Path2/privkey.pem' \
+    -out '$SSL_Path2/fullchain.pem' \
+    -subj '/CN=localhost'" certbot
 docker-compose -f docker-compose.production.yml up --force-recreate -d nginx
 
 docker-compose -f docker-compose.production.yml run --rm --entrypoint "\
@@ -18,11 +33,28 @@ docker-compose -f docker-compose.production.yml run --rm --entrypoint "\
   rm -rf /etc/letsencrypt/archive/$domain && \
   rm -rf /etc/letsencrypt/renewal/$domain.conf" certbot
 
+
+docker-compose -f docker-compose.production.yml run --rm --entrypoint "\
+  rm -rf /etc/letsencrypt/live/$domain2 && \
+  rm -rf /etc/letsencrypt/archive/$domain2 && \
+  rm -rf /etc/letsencrypt/renewal/$domain2.conf" certbot
+
 docker-compose -f docker-compose.production.yml  run --rm --entrypoint "\
     certbot certonly --webroot -w /var/www/certbot \
     --email hsm0156@gmail.com \
     -d $domain \
     -d www.$domain \
+    --rsa-key-size 4096 \
+    --agree-tos \
+    --no-eff-email \
+    --force-renewal" certbot
+
+
+docker-compose -f docker-compose.production.yml  run --rm --entrypoint "\
+    certbot certonly --webroot -w /var/www/certbot \
+    --email hsm0156@gmail.com \
+    -d $domain2 \
+    -d www.$domain2 \
     --rsa-key-size 4096 \
     --agree-tos \
     --no-eff-email \
